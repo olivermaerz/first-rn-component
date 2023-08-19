52899ddefd8bc33117b476b45874452d1b7a09d1
Oliver Maerz <7649813+olivermaerz@users.noreply.github.com>
2023-08-19 18:07:52 -0300

    Remove redundant import

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 
